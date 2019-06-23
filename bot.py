890cfaf0460c5c02495b5d7b16f339a11a4dc0eb
Tuise <34126719+Tuise233@users.noreply.github.com>
2019-06-23 11:21:02 +0800

    新增自动回复功能
    新增自动回复功能

--- bot.py
+++ bot.py
@@ -5,15 +5,15 @@ import datetime
 import time
 import json
 
-def get_sentence(api):
+def get_sentence(api): #每日一句
         sentence = requests.get(api)
         return sentence.json()
 
-def get_weather(api):
+def get_weather(api): #天气预报
         response = requests.get(api)
         return response.json()
 
-def get_trans(content):
+def get_trans(content): #翻译
         url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
         post_form = {
                 'i':content,
@@ -36,6 +36,43 @@ def get_trans(content):
         result = translate_dict['translateResult'][0][0]['tgt']
         return result
 
+def auto_chat(text):
+        url = 'http://api.qingyunke.com/api.php?key=free&appid=0&msg={}'.format(text)
+        datas={
+                "key":"free",
+                "appid":"0",
+                "msg":text
+        }
+        response = requests.post(url,data=datas)
+        html = json.loads(response.text)
+        return html['content']
+
+def get_daytips():
+        while True:
+                jsAPI = 'http://open.iciba.com/dsapi/'
+                tqAPI = 'https://www.tianqiapi.com/api/'
+                sentence = get_sentence(jsAPI)
+                content = sentence['content'] #英文句子
+                note = sentence['note']
+                weather = get_weather(tqAPI)
+                info = weather['data'][0]
+                tips = '城市:{}\n日期:{}\n空气指数:{} - {}\n天气情况:{}\n小提示:{}\n'.format(weather['city'],info['date'],info['air'],info['air_level'],info['wea'],info['air_tips'])
+                users = itchat.search_friends('as') #搜索发送UserName (崽崽 = 用户备注)
+                UserName = users[0]['UserName']
+                while True:
+                        time = datetime.datetime.now()
+                        year = time.year
+                        month = time.month
+                        day = time.day
+                        hour = time.hour
+                        minute = time.minute
+                        now = '现在是北京时间{}.{}.{} {}:{}'.format(year,month,day,hour,minute)
+                        if hour == 10 and minute == 41:
+                                string = '[崽崽BOT]\n{}\n\n每日一句:{}\n{}\n\n天气信息:\n{}'.format(now,content,note,tips)
+                                itchat.send_msg(string,UserName)
+                                break
+
+#返回用户输入的参数
 @itchat.msg_register(itchat.content.TEXT)
 def print_content(msg):
         FromUserName = msg['FromUserName']
@@ -45,32 +82,16 @@ def print_content(msg):
                 CutText = text.split('_')
                 if CutText[0] == '翻译':
                         trans = get_trans(CutText[1])
-                        print(FromUserName,ToUserName)
                         itchat.send_msg('[崽崽BOT]\n翻译结果:{}'.format(trans),toUserName = FromUserName)
+        else:
+                result = auto_chat(text)
+                if result.find('菲菲') != -1:
+                        replaced = result.replace('菲菲','崽崽')
+                        itchat.send_msg('[崽崽BOT]\n{}'.format(replaced),toUserName = FromUserName)
+                        return
+                itchat.send_msg('[崽崽BOT]\n{}'.format(result),toUserName = FromUserName)
         
-
 if __name__ == '__main__':
-        jsAPI = 'http://open.iciba.com/dsapi/'
-        tqAPI = 'https://www.tianqiapi.com/api/'
         itchat.auto_login(hotReload=True)
         itchat.run()
-        sentence = get_sentence(jsAPI)
-        content = sentence['content'] #英文句子
-        note = sentence['note']
-        weather = get_weather(tqAPI)
-        info = weather['data'][0]
-        tips = '城市:{}\n日期:{}\n空气指数:{} - {}\n天气情况:{}\n小提示:{}\n'.format(weather['city'],info['date'],info['air'],info['air_level'],info['wea'],info['air_tips'])
-        users = itchat.search_friends('佳铃') #搜索发送UserName (崽崽 = 用户备注)
-        UserName = users[0]['UserName']
-        while True:
-                time = datetime.datetime.now()
-                year = time.year
-                month = time.month
-                day = time.day
-                hour = time.hour
-                minute = time.minute
-                now = '现在是北京时间{}.{}.{} {}:{}'.format(year,month,day,hour,minute)
-                if hour == 18 and minute == 32:
-                        string = '[崽崽BOT]\n{}\n\n每日一句:{}\n{}\n\n天气信息:\n{}'.format(now,content,note,tips)
-                        itchat.send_msg(string,UserName)
-                        break
+        
