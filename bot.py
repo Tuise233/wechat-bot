50c969dbe49a4e88f9da1bc0d8ce03e200e21a9d
Tuise <34126719+Tuise233@users.noreply.github.com>
2019-09-13 09:53:13 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,27 +8,41 @@ import time
 import datetime
 import json
 
+
+friend = []
 #天气获取
 def get_weather():
-    url = 'https://www.tianqiapi.com/api/?version=v1&cityid=101230104&appid=[appid]&appsecret=[appsecret]'
+    url = 'http://t.weather.sojson.com/api/weather/city/101230104'
     html = requests.get(url)
     content = html.text
-    text = content.encode('utf-8').decode('unicode_escape')
-    jtext = json.loads(text)
-    city = jtext['city']
-    datas = jtext['data'][0]
-    date = datas['date']
-    week = datas['week']
-    wea = datas['wea']
-    air_level = datas['air_level']
-    air_tips = datas['air_tips']
-    result = "{}\n呼呼~今天已经{}啦~\n今天{}的天气是{},空气质量{}\n{}".format(date,week,city,wea,air_level,air_tips)
+    text = json.loads(content)
+    city = text['cityInfo']['city']
+    data = text['data']
+    week = data['forecast'][0]['week']
+    wendu = data['wendu']
+    shidu = data['shidu']
+    wea = data['forecast'][0]['type']
+    air_tips = data['forecast'][0]['notice'] +"~"+ data['ganmao']
+    result = "呼呼~今天已经{}啦~\n今天{}的天气是{}\n气温{} 湿度{}\n{}".format(week,city,wea,wendu,shidu,air_tips)
     return result
 
 #自动回复 (这里使用了免费机器人接口)
 def bot_reply(content):
-    url='http://i.itpk.cn/api.php?question='+content
-    html = requests.get(url)
+
+#    url='http://i.itpk.cn/api.php?question='+content
+#    html = requests.get(url)
+#    result = html.text
+#    return result
+    url = 'http://i.itpk.cn/api.php'
+    datas={
+        'question':content,
+        'limit':'5',
+        'api_key':'f29db2b9138e16680489b04b14e31a12',
+        'api_secret':'vs1wo26fh1ju',
+        'type':'普通文本'
+    }
+
+    html = requests.post(url,data=datas)
     result = html.text
     return result
 
@@ -39,17 +53,20 @@ def bot_run():
 
 #定时推送消息
 def bot_push():
-    friend = ['@5bf1ad9889d7a642dcdd7b54472629f95edd1d9d5b43815cda50488cc61be76d','@2a020a846b287bf7d19bd2664537c0527c947e80a94d7c13c893c0cdb066a301']
     while(True):
         time = datetime.datetime.now()
         hour = time.hour
         minute = time.minute
         times = "{0}{1}".format(hour,minute)
-        if(times == '1830' or times == '0700' or times == '700'):
+        if(times == '1830' or times == '952' or times == '70'):
             if(Num == 0):
                 for i in friend:
+                    print(times)
                     string = get_weather()
                     itchat.send(string,toUserName=i)
+                    stime = datetime.datetime.now()
+                    pstring = '[{}:{}]推送消息:\n{}\n给用户{}'.format(stime.hour,stime.minute,string,i)
+                    print(pstring)
                 Num = 1
         else:
             Num = 0
@@ -67,8 +84,14 @@ def auto_reply(msg):
     FromUser = msg['FromUserName']
     ToUser = msg['ToUserName']
     Text = msg['Text']
+    if(Text == 'system_get'):
+        a = itchat.search_friends('as')[0]['UserName']
+        friend.append(a)
+        print(friend)
     reply = bot_reply(Text)
-    print(reply)
+    time = datetime.datetime.now()
+    string = '[{}:{}]回复消息:{}'.format(time.hour,time.minute,reply)
+    print(string)
     itchat.send(reply,toUserName=FromUser)
 
 #Main
